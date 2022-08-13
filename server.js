ffbc57814bde1eb363f62e2e88972fdb4677c498
Jennifer Nnadi <jennify.nnadi@gmail.com>
2022-08-13 16:05:40 -0400

    added static file in server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ const port = 8000 || process.env.PORT;
 const app = express()
 app.use(cors())
 app.use(express.json())
+app.use(express.static("build"));
 
 app.get('/tickets', async(req, res) => {
     const options = {

f3bdd184fdb9a57c06ff1759d6511c3b77e5c509
Jennifer Nnadi <jennify.nnadi@gmail.com>
2022-08-13 16:35:04 -0400

    fixed static file path

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const port = 8000 || process.env.PORT;
 const app = express()
 app.use(cors())
 app.use(express.json())
-app.use(express.static("build"));
+app.use(express.static(`${__dirname}build`));
 
 app.get('/tickets', async(req, res) => {
     const options = {
